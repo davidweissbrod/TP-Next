dbbf2a9b32e28c3352310f3681a4494809b40c8e
DAVID EZEQUIEL WEISSBROD <47436755@ort>
2024-07-31 08:01:52 -0300

    foto

--- tp-next/src/app/quienes-somos/quienes-somos.js
+++ tp-next/src/app/quienes-somos/quienes-somos.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import IanDavid from '../../img/david-ian.jpg'
+import { Image } from 'react-bootstrap'
 
 const Nosotros = () =>{
     <>
         <h1>¿Quienes somos?</h1>
         <br/>
         <h2>Ian y David</h2>
-        <img src={IanDavid}/>
+        <Image src={IanDavid}/>
         <p>
             Somos dos personas que aman a los animalitos y buscamos ayudarlos de las maneras que podemos, por eso es que creamos esta pagina para nuestra 
             guardia, haciendo que sea mas facil organizar los turnos logrando asi mucho mas dinamismo y el poder ayudar a muchas mas mascotas.
